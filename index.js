17e838573a826301c8a12d89384261a367e64274
annastacia <annastaciangina96@gmail.com>
2022-06-13 23:03:04 +0300

    commit tree 29d5efc6056b53da8bf803df99eab264941738d7

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+]
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+    return sentence.split(" ").map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(" ");
+  })
 }
